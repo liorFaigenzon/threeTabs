3eb6f39658ec7ae2796f3e8d6490995b8d73373c
Alon Geffen <alongeffen@gmail.com>
2015-11-29 23:49:50 +0200

    Add About view Controller

---
+++ threeTabs/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  threeTabs
+//
+//  Created by Alon Geffen on 29/11/2015.
+//  Copyright © 2015 Faigenzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
